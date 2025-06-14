5be81e1a84fec21a97d282344e43fa0a94e2738a
fernando-mss <contatofernandomss@gmail.com>
2025-06-14 16:01:23 -0300

    teste funcional

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Aplicação Flask rodando no ECS com CI/CD! v2!"
+    return "Aplicação Flask rodando no ECS com CI/CD! v1!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
