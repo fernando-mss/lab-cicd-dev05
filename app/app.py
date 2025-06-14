54200ab4311c0e53d36153a23c0bdf7fd78da110
fernando-mss <contatofernandomss@gmail.com>
2025-06-14 15:16:20 -0300

    buildspec.yml change

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Aplicação Flask rodando no ECS com CI/CD!"
+    return "Aplicação Flask rodando no ECS com CI/CD! v2"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
